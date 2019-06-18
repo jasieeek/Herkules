f0183fe0e44858933c0942603ab377cced814112
jasieeek <kamil.jasiak@vp.pl>
2019-06-18 19:09:43 +0200

    Initial commit

---
+++ src/main/java/pl/herkules/App.java
@@ -0,0 +1,22 @@
+package pl.herkules;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/mainScene.fxml"));
+
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/pl/herkules/controller/MainController.java
@@ -0,0 +1,11 @@
+package pl.herkules.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainController {
+    @FXML
+    public void button(ActionEvent event){
+        System.out.println("Hello World!");
+    }
+}
